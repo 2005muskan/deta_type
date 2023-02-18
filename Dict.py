7c44d6fade7383ccf3034d761b5cf1daa1363c8d
2005muskan <muskankushwah131@gmail.com>
2023-02-18 07:19:50 +0530

    simple commit

---
+++ Dict.py
@@ -0,0 +1,11 @@
+dic = {"name":"G","surname":"kushwah","class":{"cla":"second year"},"college":"madhav science"}
+print(dic)
+print(type(dic))
+
+dic.items()
+
+print(dic.values())
+
+print(dic.keys())
+
+print(dic.get("surname"))
